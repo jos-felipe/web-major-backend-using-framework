3d4e66a9e2c79b564c2be8bd704c5254a341d61b
J Felipe D Stieven <jfelipedalcin@gmail.com>
2025-09-02 17:02:23 +0000

    Add basic Fastify server setup with start script

---
+++ index.js
@@ -0,0 +1,20 @@
+// Basic Fastify server setup
+const fastify = require('fastify')({ logger: true });
+
+// Declare a route
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' };
+});
+
+// Run the server
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000, host: '0.0.0.0' });
+    console.log('Server is running on http://localhost:3000');
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
